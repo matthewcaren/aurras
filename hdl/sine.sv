53218cb2629ca56bab0217cd01fccf64847db073
Jonas Rajagopal <jrajagop@mit.edu>
2023-10-31 21:28:28 -0400

    added memory module

---
+++ hdl/sine.sv
@@ -0,0 +1,101 @@
+`timescale 1ns / 1ps
+`default_nettype none // prevents system from inferring an undeclared logic (good practice)
+
+//Sine Wave Generator
+module sine_generator (
+  input wire clk_in,
+  input wire rst_in, //clock and reset
+  input wire step_in, //trigger a phase step (rate at which you run sine generator)
+  output logic signed [7:0] amp_out); //output phase in 2's complement
+
+  parameter PHASE_INCR = 32'b1000_0000_0000_0000_0000_0000_0000_0000>>3; //1/16th of 12 khz is 750 Hz
+  
+  logic [31:0] phase;
+  logic [7:0] amp;
+  logic [7:0] amp_pre;
+  assign amp_pre = ({~amp[7],amp[6:0]}); //2's comp output (if not scaling)
+  assign amp_out = amp_pre>>>4; //decrease volume so it isn't too loud!
+  sine_lut lut_1(.clk_in(clk_in), .phase_in(phase[31:26]), .amp_out(amp));
+
+  always_ff @(posedge clk_in)begin
+    if (rst_in)begin
+      phase <= 32'b0;
+    end else if (step_in)begin
+      phase <= phase+PHASE_INCR;
+    end
+  end
+endmodule
+
+//6bit sine lookup, 8bit depth
+module sine_lut(input wire [5:0] phase_in, input wire clk_in, output logic[7:0] amp_out);
+  always_ff @(posedge clk_in)begin
+    case(phase_in)
+      6'd0: amp_out<=8'd128;
+      6'd1: amp_out<=8'd140;
+      6'd2: amp_out<=8'd152;
+      6'd3: amp_out<=8'd165;
+      6'd4: amp_out<=8'd176;
+      6'd5: amp_out<=8'd188;
+      6'd6: amp_out<=8'd198;
+      6'd7: amp_out<=8'd208;
+      6'd8: amp_out<=8'd218;
+      6'd9: amp_out<=8'd226;
+      6'd10: amp_out<=8'd234;
+      6'd11: amp_out<=8'd240;
+      6'd12: amp_out<=8'd245;
+      6'd13: amp_out<=8'd250;
+      6'd14: amp_out<=8'd253;
+      6'd15: amp_out<=8'd254;
+      6'd16: amp_out<=8'd255;
+      6'd17: amp_out<=8'd254;
+      6'd18: amp_out<=8'd253;
+      6'd19: amp_out<=8'd250;
+      6'd20: amp_out<=8'd245;
+      6'd21: amp_out<=8'd240;
+      6'd22: amp_out<=8'd234;
+      6'd23: amp_out<=8'd226;
+      6'd24: amp_out<=8'd218;
+      6'd25: amp_out<=8'd208;
+      6'd26: amp_out<=8'd198;
+      6'd27: amp_out<=8'd188;
+      6'd28: amp_out<=8'd176;
+      6'd29: amp_out<=8'd165;
+      6'd30: amp_out<=8'd152;
+      6'd31: amp_out<=8'd140;
+      6'd32: amp_out<=8'd128;
+      6'd33: amp_out<=8'd115;
+      6'd34: amp_out<=8'd103;
+      6'd35: amp_out<=8'd90;
+      6'd36: amp_out<=8'd79;
+      6'd37: amp_out<=8'd67;
+      6'd38: amp_out<=8'd57;
+      6'd39: amp_out<=8'd47;
+      6'd40: amp_out<=8'd37;
+      6'd41: amp_out<=8'd29;
+      6'd42: amp_out<=8'd21;
+      6'd43: amp_out<=8'd15;
+      6'd44: amp_out<=8'd10;
+      6'd45: amp_out<=8'd5;
+      6'd46: amp_out<=8'd2;
+      6'd47: amp_out<=8'd1;
+      6'd48: amp_out<=8'd0;
+      6'd49: amp_out<=8'd1;
+      6'd50: amp_out<=8'd2;
+      6'd51: amp_out<=8'd5;
+      6'd52: amp_out<=8'd10;
+      6'd53: amp_out<=8'd15;
+      6'd54: amp_out<=8'd21;
+      6'd55: amp_out<=8'd29;
+      6'd56: amp_out<=8'd37;
+      6'd57: amp_out<=8'd47;
+      6'd58: amp_out<=8'd57;
+      6'd59: amp_out<=8'd67;
+      6'd60: amp_out<=8'd79;
+      6'd61: amp_out<=8'd90;
+      6'd62: amp_out<=8'd103;
+      6'd63: amp_out<=8'd115;
+    endcase
+  end
+endmodule
+
+`default_nettype wire
--- hdl/top_level.sv
+++ hdl/top_level.sv
@@ -1,12 +1,16 @@
 module top_level(
     input wire clk_100mhz,
     input wire [15:0] sw,
-    output logic [15:0] led,
+    
     input wire [7:0] pmoda, // Tbd how many bits actually needed
-    input wire [2:0] pmodb,
+    input wire [7:0] pmodb, // Tbd how many bits actually needed
     input wire [3:0] btn,
 
     output logic spkl, spkr, //speaker outputs
+    output logic [15:0] led, // led outputs
+
+    output logic uart_txd, // if we want to use Manta
+    input wire uart_rxd
 
 )
     assign led = sw;
