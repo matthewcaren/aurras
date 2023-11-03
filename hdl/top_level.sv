8f3d11ea285f91dc8d722361a364c99d441df626
Bryan Jangeesingh <brytech@mit.edu>
2023-11-03 14:02:50 -0400

    made new module and got spkl and spkr working separately 1

--- hdl/top_level.sv
+++ hdl/top_level.sv
@@ -18,47 +18,19 @@ module top_level(
 
 );
 
-    assign rgb1= 0;
-    assign rgb0 = 0;
-    // logic [1:0] mic_select;
+  logic sys_rst;
 
-    // assign mic_select = sw[15:14];
-    // always_comb begin
-    //     case: (mic_select)
-    //         2'b10: // Select mic 1
-    //         2'b11: // Select mic 2
-    //         2'b00: // Select mic 3
-    //     endcase
-    // end
-
+  assign rgb1= 0;
+  assign rgb0 = 0;
 
   logic clk_m;
-  audio_clk_wiz macw (.clk_in(clk_100mhz), .clk_out(clk_m)); //98.3MHz
-  // we make 98.3 MHz since that number is cleanly divisible by
-  // 32 to give us 3.072 MHz.  3.072 MHz is nice because it is cleanly divisible
-  // by nice powers of 2 to give us reasonable audio sample rates. For example,
-  // a decimation by a factor of 64 could give us 6 bit 48 kHz audio
-  // a decimation by a factor of 256 gives us 8 bit 12 kHz audio
-  //we do the latter in this lab.
-
-
-  //logic for controlling PDM associated modules:
-
-  logic audio_sample_valid;//single-cycle enable for samples at ~12 kHz (approx)
-  logic[7:0] audio_data; //raw scaled audio data
-
-  //logic to produce 25 MHz step signal for PWM module
-  logic [1:0] pwm_counter;
-  logic pwm_step; //single-cycle pwm step
-  assign pwm_step = (pwm_counter==2'b11);
-
-  always_ff @(posedge clk_m)begin
-    pwm_counter <= pwm_counter+1;
-  end
-
+  audio_clk_wiz macw (.clk_in(clk_100mhz), .clk_out(clk_m));
+  
+ // ############################################################## Set up the sound sources - START
 
   localparam integer COUNTER_MAX = 8192; // This is 2^13, which divides 98.3MHz down to ~12kHz
   logic [12:0] counter; // 13-bit counter to divide down the clock
+  logic audio_sample_valid;//single-cycle enable for samples at ~12 kHz (approx)
 
   always @(posedge clk_m) begin
       if (counter == COUNTER_MAX - 1) begin
@@ -70,8 +42,8 @@ module top_level(
       end
     end
 
-  logic [7:0] tone_750; //output of sine wave of 750Hz
-  logic [7:0] tone_440; //output of sine wave of 440 Hz
+  logic [7:0] tone_750; 
+  logic [7:0] tone_440; 
 
   sine_generator sine_750 (
     .clk_in(clk_m),
@@ -88,52 +60,23 @@ module top_level(
   ); 
 
   defparam sine_440.PHASE_INCR = 32'b1001_0110_0010_1111_1100_1001_0110;
-  //2^32/(12000/440) = 157,482,134.2 
 
-
-  //choose which signal to play:
-  logic [7:0] audio_data_sel;
-
-  always_comb begin
-    if (sw[0])begin
-      audio_data_sel = tone_750; //signed
-    end else if (sw[1])begin
-      audio_data_sel = tone_440; //signed
-    end
-  end
-
-
-  logic signed [7:0] vol_out;
-  volume_control vc (.vol_in(sw[15:13]),.signal_in(audio_data_sel), .signal_out(vol_out));
-
-  logic pwm_out_signal;
-
-  pwm my_pwm(
+  // ############################################################## Set up the sound sources - END 
+  
+  logic sound_out; 
+  audio_player ap_1 (
     .clk_in(clk_m),
-    .rst_in(sys_rst),
-    .level_in(vol_out),
-    .tick_in(pwm_step),
-    .pwm_out(pwm_out_signal)
+    .sound_sample_in(tone_750), 
+    .signal_out(sound_out),
+    .sw(sw)
   );
 
- 
-  assign spkl = pwm_out_signal;
-  assign spkr = pwm_out_signal;
+
+  assign spkl = sw[0]? sound_out : 0;
+  assign spkr = sw[1]? sound_out : 0;
 
 endmodule // top_level
 
-//Volume Control
-module volume_control (
-  input wire [2:0] vol_in,
-  input wire signed [7:0] signal_in,
-  output logic signed [7:0] signal_out);
-    logic [2:0] shift;
-    assign shift = 3'd7 - vol_in;
-    assign signal_out = signal_in>>>shift;
-endmodule
-
-
-    
 
     
     
