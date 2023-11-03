a5bc6ef4378b915a4f2dd837988c2efb5b71734c
Bryan Jangeesingh <brytech@mit.edu>
2023-11-03 02:46:20 -0400

    working with the option to have either pdm or pwm

--- hdl/top_level.sv
+++ hdl/top_level.sv
@@ -1,106 +1,200 @@
 module top_level(
     input wire clk_100mhz,
     input wire [15:0] sw,
-    input wire [7:0] pmoda, // Input wires from the mics (data)
-    output logic [7:0] pmodb, // Output wires to the mics (clocks)
+    
+    input wire [7:0] pmoda, // Tbd how many bits actually needed
+    input wire [7:0] pmodb, // Tbd how many bits actually needed
     input wire [3:0] btn,
-    output logic [6:0] ss0_c, ss1_c,
-    output logic [3:0] ss0_an, ss1_an,
-    output logic [2:0] rgb0, rgb1, //rgb led
+
     output logic spkl, spkr, //speaker outputs
     output logic [15:0] led, // led outputs
+
     output logic uart_txd, // if we want to use Manta
     input wire uart_rxd,
     output logic mic_clk, //microphone clock
     input wire  mic_data //microphone data
 
-    assign led = sw;
-    logic sys_rst;
-    assign sys_rst = btn[0];
-    assign rgb1 = 0;
+);
+
+    assign rgb1= 0;
     assign rgb0 = 0;
+    logic [1:0] mic_select;
 
-    // 98.3MHz
-    logic audio_clk;
-    audio_clk_wiz macw (.clk_in(clk_100mhz), .clk_out(audio_clk)); 
-
-    // This triggers at 24kHz for general audio
-    logic audio_trigger;
-    logic [11:0] counter;
-    always_ff @(posedge audio_clk) begin
-        counter <= counter + 1;
+    assign mic_select = sw[15:14];
+    always_comb begin
+        case (mic_select)
+            2'b10: // Select mic 1
+            2'b11: // Select mic 2
+            2'b00: // Select mic 3
+            default: 
+        endcase
     end
-    assign audio_trigger = (counter == 0);
 
-    //3.072MHz clock to send to i2s
-    logic [4:0] i2s_clk_counter;
-    logic i2s_clk;
-    always_ff @(posedge audio_clk) begin
-        i2s_clk_counter <= i2s_clk_counter + 1;
+
+  logic clk_m;
+  audio_clk_wiz macw (.clk_in(clk_100mhz), .clk_out(clk_m)); //98.3MHz
+  // we make 98.3 MHz since that number is cleanly divisible by
+  // 32 to give us 3.072 MHz.  3.072 MHz is nice because it is cleanly divisible
+  // by nice powers of 2 to give us reasonable audio sample rates. For example,
+  // a decimation by a factor of 64 could give us 6 bit 48 kHz audio
+  // a decimation by a factor of 256 gives us 8 bit 12 kHz audio
+  //we do the latter in this lab.
+
+
+  //logic for controlling PDM associated modules:
+  logic [8:0] m_clock_counter; //used for counting for mic clock generation
+  logic audio_sample_valid;//single-cycle enable for samples at ~12 kHz (approx)
+  logic signed [7:0] mic_audio; //audio from microphone 8 bit unsigned at 12 kHz
+  logic[7:0] audio_data; //raw scaled audio data
+
+  //logic for interfacing with the microphone and generating 3.072 MHz signals
+  logic [7:0] pdm_tally;
+  logic [8:0] pdm_counter;
+
+  localparam PDM_COUNT_PERIOD = 32; //do not change
+  localparam NUM_PDM_SAMPLES = 256; //number of pdm in downsample/decimation/average
+
+  logic old_mic_clk; //prior mic clock for edge detection
+  logic sampled_mic_data; //one bit grabbed/held values of mic
+  logic pdm_signal_valid; //single-cycle signal at 3.072 MHz indicating pdm steps
+
+  assign pdm_signal_valid = mic_clk && ~old_mic_clk;
+
+
+  //logic to produce 25 MHz step signal for PWM module
+  logic [1:0] pwm_counter;
+  logic pwm_step; //single-cycle pwm step
+  assign pwm_step = (pwm_counter==2'b11);
+
+  always_ff @(posedge clk_m)begin
+    pwm_counter <= pwm_counter+1;
+  end
+
+  //generate clock signal for microphone
+  //microphone signal at ~3.072 MHz
+  always_ff @(posedge clk_m)begin
+    mic_clk <= m_clock_counter < PDM_COUNT_PERIOD/2;
+    m_clock_counter <= (m_clock_counter==PDM_COUNT_PERIOD-1)?0:m_clock_counter+1;
+    old_mic_clk <= mic_clk;
+  end
+  
+  //generate audio signal (samples at ~12 kHz
+  always_ff @(posedge clk_m)begin
+    if (pdm_signal_valid)begin
+      sampled_mic_data    <= mic_data;
+      pdm_counter         <= (pdm_counter==NUM_PDM_SAMPLES)?0:pdm_counter + 1;
+      pdm_tally           <= (pdm_counter==NUM_PDM_SAMPLES)?mic_data
+                                                            :pdm_tally+mic_data;
+      audio_sample_valid  <= (pdm_counter==NUM_PDM_SAMPLES);
+      mic_audio           <= (pdm_counter==NUM_PDM_SAMPLES)?{~pdm_tally[7],pdm_tally[6:0]}
+                                                            :mic_audio;
+    end else begin
+      audio_sample_valid <= 0;
     end
-    assign i2s_clk = i2s_clk_counter[4];
+  end
+
+  logic [7:0] tone_750; //output of sine wave of 750Hz
+  logic [7:0] tone_440; //output of sine wave of 440 Hz
+  logic [7:0] single_audio; //recorder non-echo output
+  logic [7:0] echo_audio; //recorder echo output
+
+
+  sine_generator sine_750 (
+    .clk_in(clk_m),
+    .rst_in(sys_rst),
+    .step_in(audio_sample_valid),
+    .amp_out(tone_750)
+  ); 
+
+  sine_generator sine_440 (
+    .clk_in(clk_m),
+    .rst_in(sys_rst),
+    .step_in(audio_sample_valid),
+    .amp_out(tone_440)
+  ); 
+
+  defparam sine_440.PHASE_INCR = 32'b1001_0110_0010_1111_1100_1001_0110;
+  //2^32/(12000/440) = 157,482,134.2 
+
+
+  //choose which signal to play:
+  logic [7:0] audio_data_sel;
+
+  always_comb begin
+    if (sw[0])begin
+      audio_data_sel = tone_750; //signed
+    end else if (sw[1])begin
+      audio_data_sel = tone_440; //signed
+    end else if (sw[5])begin
+      audio_data_sel = mic_audio; //signed
+    end else if (sw[6])begin
+      audio_data_sel = single_audio; //signed
+    end else if (sw[7])begin
+      audio_data_sel = echo_audio; //signed
+    end else begin
+      audio_data_sel = mic_audio; //signed
+    end
+  end
+
+
+  logic signed [7:0] vol_out; //can be signed or not signed...doesn't really matter
+  // all this does is convey the output of vol_out to the input of the pdm
+  // since it isn't used directly with any sort of math operation its signedness
+  // is not as important.
+  volume_control vc (.vol_in(sw[15:13]),.signal_in(audio_data_sel), .signal_out(vol_out));
+
+  //PWM:
+  logic pwm_out_signal; //an inherently digital signal (0 or 1..no need to make signed)
+  //the "value" is encoded using Pulse Width Modulation
+  //PDM:
+  logic pdm_out_signal; //an inherently digital signal (0 or 1..no need to make signed)
+  //the value is encoded using Pulse Density Modulation
+  logic audio_out; //value that drives output channels directly
+
+  //already implemented for you:
+  pwm my_pwm(
+    .clk_in(clk_m),
+    .rst_in(sys_rst),
+    .level_in(vol_out),
+    .tick_in(pwm_step),
+    .pwm_out(pwm_out_signal)
+  );
+
+  //you build (currently empty):
+  pdm my_pdm(
+    .clk_in(clk_m),
+    .rst_in(sys_rst),
+    .level_in(vol_out),
+    .tick_in(pdm_signal_valid),
+    .pdm_out(pdm_out_signal)
+  );
+
+  always_comb begin
+    case (sw[4:3])
+      2'b00: audio_out = pwm_out_signal;
+      2'b01: audio_out = pdm_out_signal;
+      2'b10: audio_out = sampled_mic_data;
+      2'b11: audio_out = 0;
+    endcase
+  end
+
+  assign spkl = audio_out;
+  assign spkr = audio_out;
+
+endmodule // top_level
+
+//Volume Control
+module volume_control (
+  input wire [2:0] vol_in,
+  input wire signed [7:0] signal_in,
+  output logic signed [7:0] signal_out);
+    logic [2:0] shift;
+    assign shift = 3'd7 - vol_in;
+    assign signal_out = signal_in>>>shift;
+endmodule
+
+
     
-    // 48kHz clock for word select for i2s
-    logic [10:0] lrcl_counter;
-    logic lrcl_clk; 
-    always_ff @(posedge audio_clk) begin
-        lrcl_counter <= lrcl_counter + 1;
-    end
-    assign lrcl_clk = lrcl_counter[10];
 
-    logic mic_1_data, mic_2_data, mic_3_data;
-
-    // Mic 1: blck - i2s_clk - pmodb[3]; dout - mic_1_data - pmoda[3]; lrcl_clk - pmodb[2], sel - grounded
-    // Mic 2: blck - i2s_clk - pmodb[7]; dout - mic_2_data - pmoda[7]; lrcl_clk - pmodb[6], sel - grounded
-    // Mic 3: blck - i2s_clk - pmodb[1]; dout - mic_3_data - pmoda[0]; lrcl_clk - pmodb[0], sel - grounded
-
-    assign pmodb[3] = i2s_clk;
-    // assign pmodb[7] = i2s_clk;
-    // assign pmodb[1] = i2s_clk;
-
-    assign pmodb[2] = lrcl_clk;
-    // assign pmodb[6] = lrcl_clk;
-    // assign pmodb[0] = lrcl_clk;
-
-    assign mic_1_data = pmoda[3];
-    // assign mic_2_data = pmoda[7];
-    // assign mic_3_data = pmoda[0];
-
-    // Audio out sends data_valid_out signal at 48kHz
-    logic [15:0] audio_out_1, audio_out_2, audio_out_3;
-    logic [15:0] valid_audio_out_1, valid_audio_out_2, valid_audio_out_3;
-    logic data_valid_out_1, data_valid_out_2, data_valid_out_3;
-
-    i2s mic_1(.mic_data(mic_1_data), .i2s_clk(i2s_clk), .lrcl_clk(lrcl_clk), .data_valid_out(data_valid_out_1), .audio_out(audio_out_1));
-    // i2s mic_2(.mic_data(mic_2_data), .i2s_clk(i2s_clk), .lrcl_clk(lrcl_clk), .data_valid_out(data_valid_out_2), .audio_out(audio_out_2));
-    // i2s mic_3(.mic_data(mic_3_data), .i2s_clk(i2s_clk), .lrcl_clk(lrcl_clk), .data_valid_out(data_valid_out_3), .audio_out(audio_out_3));
-
-    always_ff @(posedge audio_clk) begin
-        if (data_valid_out_1) begin
-            valid_audio_out_1 <= audio_out_1;
-        end
-        // if (data_valid_out_2) begin
-        //     valid_audio_out_2 <= audio_out_2;
-        // end
-        // if (data_valid_out_3) begin
-        //     valid_audio_out_3 <= audio_out_3;
-        // end
-    end
-
-
-    // seven segment display - display valid_audio_out_1
-    logic [31:0] prev_val, val_to_display;
-    always_ff @(posedge audio_clk) begin
-        prev_val <= val_to_display;
-    end
-    assign val_to_display = btn[1] ? (sw[7] ? valid_audio_out_1 : 16'b0) : prev_val;
-    logic [6:0] ss_c;
-    assign ss0_c = ss_c; 
-    assign ss1_c = ss_c;
-    seven_segment_controller mssc(.clk_in(audio_clk),
-                                .rst_in(sys_rst),
-                                .val_in(val_to_display),
-                                .cat_out(ss_c),
-                                .an_out({ss0_an, ss1_an}));
-
-endmodule 
+    
+    
