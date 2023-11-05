63f11b08f032a13b09c1a715232fe30d9236548e
Jonas Rajagopal <jrajagop@mit.edu>
2023-11-04 21:15:43 -0400

    Named pmod stuff

--- hdl/i2s.sv
+++ hdl/i2s.sv
@@ -2,8 +2,8 @@ module i2s(
     input wire mic_data,
     input wire i2s_clk,
     input wire lrcl_clk,
-    output logic data_valid,
-    output logic [63:0] data_out
+    output logic data_valid_out,
+    output logic [63:0] full_audio_out
 );
 
 logic [5:0] current_address;
@@ -11,17 +11,17 @@ logic prev_lrcl_clk;
 logic [63:0] full_audio;
 always_ff @(posedge i2s_clk) begin
     if (~(prev_lrcl_clk) && lrcl_clk) begin
-        data_out <= full_audio;
-        data_valid <= 1;
+        full_audio_out <= full_audio;
+        data_valid_out <= 1;
         full_audio[63] <= mic_data;
         current_address <= 1;
     end else begin
         current_address <= current_address + 1;
         full_audio[63-current_address] <= mic_data;
     end
-    if (data_valid) begin
-        data_valid <= 0;
-        data_out <= 0;
+    if (data_valid_out) begin
+        data_valid_out <= 0;
+        full_audio_out <= 0;
     end
     prev_lrcl_clk <= lrcl_clk;
 end
--- hdl/top_level.sv
+++ hdl/top_level.sv
@@ -2,9 +2,8 @@ module top_level(
     input wire clk_100mhz,
     input wire [15:0] sw,
 
-
-    input wire [7:0] pmoda, // Tbd how many bits actually needed
-    output logic [7:0] pmodb, // Tbd how many bits actually needed
+    input wire [7:0] pmoda, // Input wires from the mics (data)
+    output logic [7:0] pmodb, // Output wires to the mics (clocks)
 
     input wire [3:0] btn,
     output logic [6:0] ss0_c,
@@ -27,57 +26,58 @@ module top_level(
 
     audio_clk_wiz macw (.clk_in(clk_100mhz), .clk_out(audio_clk)); //98.3MHz
 
+    // This triggers at 24kHz for general audio
     logic [11:0] counter;
     always_ff @(posedge audio_clk) begin
         counter <= counter + 1;
     end
-    assign audio_trigger = (counter == 0); // This triggers at 24kHz
+    assign audio_trigger = (counter == 0);
 
+    //3.072MHz clock to send to i2s
     logic [4:0] i2s_clk_counter;
-    logic i2s_clk; //3.072MHz
+    logic i2s_clk;
     always_ff @(posedge audio_clk) begin
         i2s_clk_counter <= i2s_clk_counter + 1;
     end
     assign i2s_clk = i2s_clk_counter[4];
     
+    // 48kHz clock for word select for i2s
     logic [10:0] lrcl_counter;
-    logic lrcl_clk; // 48kHz
+    logic lrcl_clk; 
     always_ff @(posedge audio_clk) begin
         lrcl_counter <= lrcl_counter + 1;
     end
     assign lrcl_clk = lrcl_counter[10];
 
-    logic mic_1_data;
-    logic mic_2_data;
-    logic mic_3_data;
+    logic mic_1_data, mic_2_data, mic_3_data;
 
-    // Mic 1: blck - i2s_clk - pmodb[3], dout - pmodb[2], lrcl - pmodb[1], sel - grounded
-    // Mic 2: blck - i2s_clk - pmodb[7], dout - pmodb[6], lrcl - pmodb[5], sel - grounded
-    // Mic 3: blck - i2s_clk - pmoda[3], dout - pmoda[2], lrcl - pmoda[1], sel - grounded
+    // Mic 1: blck - i2s_clk - pmodb[3]; dout - mic_1_data - pmoda[3]; lrcl_clk - pmodb[2], sel - grounded
+    // Mic 2: blck - i2s_clk - pmodb[7]; dout - mic_2_data - pmoda[7]; lrcl_clk - pmodb[6], sel - grounded
+    // Mic 3: blck - i2s_clk - pmodb[1]; dout - mic_3_data - pmoda[0]; lrcl_clk - pmodb[0], sel - grounded
 
     assign pmodb[3] = i2s_clk;
     // assign pmodb[7] = i2s_clk;
-    // assign pmoda[3] = i2s_clk;
+    // assign pmodb[1] = i2s_clk;
 
-    assign pmodb[1] = lrcl_clk;
-    // assign pmodb[5] = lrcl_clk;
-    // assign pmoda[1] = lrcl_clk;
+    assign pmodb[2] = lrcl_clk;
+    // assign pmodb[6] = lrcl_clk;
+    // assign pmodb[0] = lrcl_clk;
 
     assign mic_1_data = pmoda[3];
-    // assign mic_2_data = pmodb[6];
-    // assign mic_3_data = pmoda[2];
+    // assign mic_2_data = pmoda[7];
+    // assign mic_3_data = pmoda[0];
 
-    logic [63:0] audio_out;
-    logic [15:0] actual_audio_out;
-    logic data_valid;
+    logic [63:0] full_audio_out_1, full_audio_out_2, full_audio_out_3;
+    logic [15:0] actual_audio_out_1, actual_audio_out_2, actual_audio_out_3;
+    logic data_valid_out_1, data_valid_out_2, data_valid_out_3;
 
-    i2s mic_1(.mic_data(mic_1_data), .i2s_clk(i2s_clk), .lrcl_clk(lrcl_clk), .data_valid(data_valid), .data_out(audio_out));
-    // i2s mic_2(.mic_data(mic_2_data), .i2s_clk(i2s_clk), .lrcl_clk(lrcl_clk), .data_valid(), .full_audio());
-    // i2s mic_3(.mic_data(mic_3_data), .i2s_clk(i2s_clk), .lrcl_clk(lrcl_clk), .data_valid(), .full_audio());
+    i2s mic_1(.mic_data(mic_1_data), .i2s_clk(i2s_clk), .lrcl_clk(lrcl_clk), .data_valid_out(data_valid_out_1), .full_audio_out(full_audio_out_1));
+    // i2s mic_1(.mic_data(mic_1_data), .i2s_clk(i2s_clk), .lrcl_clk(lrcl_clk), .data_valid_out(data_valid_out_2), .full_audio_out(full_audio_out_2));
+    // i2s mic_1(.mic_data(mic_1_data), .i2s_clk(i2s_clk), .lrcl_clk(lrcl_clk), .data_valid_out(data_valid_out_3), .full_audio_out(full_audio_out_3));
 
     always_ff @(posedge audio_clk) begin
         if (data_valid) begin
-            actual_audio_out <= audio_out[30:13];
+            actual_audio_out_1 <= full_audio_out_1[30:13];
         end
     end 
 
@@ -86,11 +86,9 @@ module top_level(
     always_ff @(posedge audio_clk) begin
         prev_val <= val_to_display;
     end
-    assign val_to_display = btn[1] ? (sw[7] ? actual_audio_out : 32'b0) : prev_val;
+    assign val_to_display = btn[1] ? (sw[7] ? actual_audio_out_1 : 32'b0) : prev_val;
     
-
-
-
+    // seven segment display
     logic [6:0] ss_c;
     assign ss0_c = ss_c; //control upper four digit's cathodes!
     assign ss1_c = ss_c; //same as above but for lower four digits!
@@ -99,6 +97,5 @@ module top_level(
                                 .val_in(val_to_display),
                                 .cat_out(ss_c),
                                 .an_out({ss0_an, ss1_an}));
-
 
 endmodule 
