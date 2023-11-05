ed97f548b1b6d41d82f645cb2e84f61afd761aab
Jonas Rajagopal <jrajagop@mit.edu>
2023-11-05 13:51:03 -0500

    Finished testing

--- hdl/top_level.sv
+++ hdl/top_level.sv
@@ -52,46 +52,43 @@ module top_level(
     // Mic 2: blck - i2s_clk - pmodb[7]; dout - mic_2_data - pmoda[7]; lrcl_clk - pmodb[6], sel - grounded
     // Mic 3: blck - i2s_clk - pmodb[1]; dout - mic_3_data - pmoda[0]; lrcl_clk - pmodb[0], sel - grounded
 
-   // assign pmodb[3] = i2s_clk;
-    assign pmodb[7] = i2s_clk;
+    assign pmodb[3] = i2s_clk;
+    // assign pmodb[7] = i2s_clk;
     // assign pmodb[1] = i2s_clk;
 
-    //assign pmodb[2] = lrcl_clk;
-    assign pmodb[6] = lrcl_clk;
+    assign pmodb[2] = lrcl_clk;
+    // assign pmodb[6] = lrcl_clk;
     // assign pmodb[0] = lrcl_clk;
 
-   // assign mic_1_data = pmoda[3];
-    assign mic_2_data = pmoda[7];
+    assign mic_1_data = pmoda[3];
+    // assign mic_2_data = pmoda[7];
     // assign mic_3_data = pmoda[0];
 
     logic [15:0] full_audio_out_1, full_audio_out_2, full_audio_out_3;
     logic [15:0] actual_audio_out_1, actual_audio_out_2, actual_audio_out_3;
     logic data_valid_out_1, data_valid_out_2, data_valid_out_3;
 
-    //i2s mic_1(.mic_data(mic_1_data), .i2s_clk(i2s_clk), .lrcl_clk(lrcl_clk), .data_valid_out(data_valid_out_1), .full_audio_out(full_audio_out_1));
-    i2s mic_2(.mic_data(mic_2_data), .i2s_clk(i2s_clk), .lrcl_clk(lrcl_clk), .data_valid_out(data_valid_out_2), .full_audio_out(full_audio_out_2));
-    // i2s mic_3(.mic_data(mic__data), .i2s_clk(i2s_clk), .lrcl_clk(lrcl_clk), .data_valid_out(data_valid_out_3), .full_audio_out(full_audio_out_3));
+    i2s mic_1(.mic_data(mic_1_data), .i2s_clk(i2s_clk), .lrcl_clk(lrcl_clk), .data_valid_out(data_valid_out_1), .full_audio_out(full_audio_out_1));
+    // i2s mic_2(.mic_data(mic_2_data), .i2s_clk(i2s_clk), .lrcl_clk(lrcl_clk), .data_valid_out(data_valid_out_2), .full_audio_out(full_audio_out_2));
+    // i2s mic_3(.mic_data(mic_3_data), .i2s_clk(i2s_clk), .lrcl_clk(lrcl_clk), .data_valid_out(data_valid_out_3), .full_audio_out(full_audio_out_3));
 
     always_ff @(posedge audio_clk) begin
-        // if (data_valid_out_1) begin
-        //     actual_audio_out_1 <= full_audio_out_1;
-        // end
-        if (data_valid_out_2) begin
-            actual_audio_out_2 <= full_audio_out_2;
+        if (data_valid_out_1) begin
+            actual_audio_out_1 <= full_audio_out_1;
         end
+        // if (data_valid_out_2) begin
+        //     actual_audio_out_2 <= full_audio_out_2;
+        // end
         // if (data_valid_out_3) begin
         //     actual_audio_out_3 <= full_audio_out_3;
         // end
-
-    end 
+    end
 
     logic [31:0] prev_val, val_to_display;
     always_ff @(posedge audio_clk) begin
         prev_val <= val_to_display;
     end
-   // assign val_to_display[15:0] = btn[1] ? (sw[7] ? actual_audio_out_1 : 16'b0) : prev_val;
-    assign val_to_display = btn[1] ? (sw[7] ? actual_audio_out_2 : 16'b0) : prev_val;
-    
+    assign val_to_display = btn[1] ? (sw[7] ? actual_audio_out_1 : 16'b0) : prev_val;
 
     // seven segment display
     logic [6:0] ss_c;
