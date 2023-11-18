b783e71751d2edf05dc8d00a1f19a8e83690e923
Matthew Caren <matt.t.caren@gmail.com>
2023-11-18 14:45:44 -0500

    rearrange top_level, correct SOS switch routing

--- hdl/top_level.sv
+++ hdl/top_level.sv
@@ -13,70 +13,91 @@ module top_level(
     input wire uart_rxd
 );
 
-    assign led = sw;
-    logic sys_rst;
-    assign sys_rst = btn[0];
-    assign rgb1 = 0;
-    assign rgb0 = 0;
+  assign led = sw;
+  logic sys_rst;
+  assign sys_rst = btn[0];
+  assign rgb1 = 0;
+  assign rgb0 = 0;
 
-    // 98.3MHz
-    logic audio_clk;
-    audio_clk_wiz macw (.clk_in(clk_100mhz), .clk_out(audio_clk)); 
+  // 98.3MHz
+  logic audio_clk;
+  audio_clk_wiz macw (.clk_in(clk_100mhz), .clk_out(audio_clk)); 
 
-    // This triggers at 24kHz for general audio
-    logic audio_trigger;
-    logic [11:0] counter;
-    always_ff @(posedge audio_clk) begin
-        counter <= counter + 1;
+  // This triggers at 24kHz for general audio
+  logic audio_trigger;
+  logic [11:0] counter;
+  always_ff @(posedge audio_clk) begin
+      counter <= counter + 1;
+  end
+  assign audio_trigger = (counter == 0);
+
+  // I2S MIC PIN ASSIGNMENTS
+  // Mic 1: bclk - i2s_clk - pmodb[3]; dout - mic_1_data - pmoda[3]; lrcl_clk - pmodb[2], sel - grounded
+  // Mic 2: bclk - i2s_clk - pmodb[7]; dout - mic_2_data - pmoda[7]; lrcl_clk - pmodb[6], sel - grounded
+  // Mic 3: bclk - i2s_clk - pmodb[1]; dout - mic_3_data - pmoda[0]; lrcl_clk - pmodb[0], sel - grounded
+
+  logic mic_1_data, mic_2_data, mic_3_data;
+  logic i2s_clk_1, i2s_clk_2, i2s_clk_3;
+  logic lrcl_clk_1, lrcl_clk_2, lrcl_clk_3;
+
+  logic [15:0] audio_out_1, audio_out_2, audio_out_3;
+
+  logic data_valid_out_1, data_valid_out_2, data_valid_out_3;
+
+  i2s mic_1(.audio_clk(audio_clk), .rst_in(sys_rst), .mic_data(mic_1_data), .i2s_clk(i2s_clk_1), .lrcl_clk(lrcl_clk_1), .data_valid_out(data_valid_out_1), .audio_out(audio_out_1));
+  i2s mic_2(.audio_clk(audio_clk), .rst_in(sys_rst), .mic_data(mic_2_data), .i2s_clk(i2s_clk_2), .lrcl_clk(lrcl_clk_2), .data_valid_out(data_valid_out_2), .audio_out(audio_out_2));
+  i2s mic_3(.audio_clk(audio_clk), .rst_in(sys_rst), .mic_data(mic_3_data), .i2s_clk(i2s_clk_3), .lrcl_clk(lrcl_clk_3), .data_valid_out(data_valid_out_3), .audio_out(audio_out_3));
+
+  assign pmodb[3] = i2s_clk_1;
+  assign pmodb[7] = i2s_clk_2;
+  assign pmodb[1] = i2s_clk_3;
+
+  assign pmodb[2] = lrcl_clk_1;
+  assign pmodb[6] = lrcl_clk_2;
+  assign pmodb[0] = lrcl_clk_3;
+
+  assign mic_1_data = pmoda[3];
+  assign mic_2_data = pmoda[7];
+  assign mic_3_data = pmoda[0];
+
+  logic [15:0] valid_audio_out_1, valid_audio_out_2, valid_audio_out_3;
+
+  always_ff @(posedge audio_clk) begin
+      if (data_valid_out_1) begin
+          valid_audio_out_1 <= audio_out_1;
+      end
+      if (data_valid_out_2) begin
+          valid_audio_out_2 <= audio_out_2;
+      end
+      if (data_valid_out_3) begin
+          valid_audio_out_3 <= audio_out_3;
+      end
+  end
+  
+  // #### INPUT ANTI-ALIASING
+  logic [15:0] filter_output;
+  logic filter_valid;
+  input_anti_alias_fir anti_alias_filter(.aclk(audio_clk),
+                                  .s_axis_data_tvalid(data_valid_out_1),
+                                  .s_axis_data_tready(1'b1),
+                                  .s_axis_data_tdata(audio_out_1),
+                                  .m_axis_data_tvalid(filter_valid),
+                                  .m_axis_data_tdata(filter_output));
+
+  logic down_sampler;
+  logic [15:0] down_sampled_audio;
+  always_ff @(posedge audio_clk) begin
+    if (filter_valid) begin
+      down_sampler <= down_sampler + 1;
+      if (down_sampler) begin
+        down_sampled_audio <= filter_output;
+      end
     end
-    assign audio_trigger = (counter == 0);
-
-    // I2S MIC PIN ASSIGNMENTS
-    // Mic 1: bclk - i2s_clk - pmodb[3]; dout - mic_1_data - pmoda[3]; lrcl_clk - pmodb[2], sel - grounded
-    // Mic 2: bclk - i2s_clk - pmodb[7]; dout - mic_2_data - pmoda[7]; lrcl_clk - pmodb[6], sel - grounded
-    // Mic 3: bclk - i2s_clk - pmodb[1]; dout - mic_3_data - pmoda[0]; lrcl_clk - pmodb[0], sel - grounded
-
-    logic mic_1_data, mic_2_data, mic_3_data;
-    logic i2s_clk_1, i2s_clk_2, i2s_clk_3;
-    logic lrcl_clk_1, lrcl_clk_2, lrcl_clk_3;
-
-    logic [15:0] audio_out_1, audio_out_2, audio_out_3;
-
-    logic data_valid_out_1, data_valid_out_2, data_valid_out_3;
-
-    i2s mic_1(.audio_clk(audio_clk), .rst_in(sys_rst), .mic_data(mic_1_data), .i2s_clk(i2s_clk_1), .lrcl_clk(lrcl_clk_1), .data_valid_out(data_valid_out_1), .audio_out(audio_out_1));
-    i2s mic_2(.audio_clk(audio_clk), .rst_in(sys_rst), .mic_data(mic_2_data), .i2s_clk(i2s_clk_2), .lrcl_clk(lrcl_clk_2), .data_valid_out(data_valid_out_2), .audio_out(audio_out_2));
-    i2s mic_3(.audio_clk(audio_clk), .rst_in(sys_rst), .mic_data(mic_3_data), .i2s_clk(i2s_clk_3), .lrcl_clk(lrcl_clk_3), .data_valid_out(data_valid_out_3), .audio_out(audio_out_3));
-
-    assign pmodb[3] = i2s_clk_1;
-    assign pmodb[7] = i2s_clk_2;
-    assign pmodb[1] = i2s_clk_3;
-
-    assign pmodb[2] = lrcl_clk_1;
-    assign pmodb[6] = lrcl_clk_2;
-    assign pmodb[0] = lrcl_clk_3;
-
-    assign mic_1_data = pmoda[3];
-    assign mic_2_data = pmoda[7];
-    assign mic_3_data = pmoda[0];
-
-    logic [15:0] valid_audio_out_1, valid_audio_out_2, valid_audio_out_3;
-
-    always_ff @(posedge audio_clk) begin
-        if (data_valid_out_1) begin
-            valid_audio_out_1 <= audio_out_1;
-        end
-        if (data_valid_out_2) begin
-            valid_audio_out_2 <= audio_out_2;
-        end
-        if (data_valid_out_3) begin
-            valid_audio_out_3 <= audio_out_3;
-        end
-    end
+  end
 
 
 
-    // ##### SPEED OF SOUND #####
+  // ##### SPEED OF SOUND #####
 
   logic sos_trigger;
   logic last_switch_val;
@@ -84,8 +105,8 @@ module top_level(
   logic [7:0] calculated_delay;
 
   always_ff @(posedge audio_clk) begin
-    sos_trigger <= sw[0] & !last_switch_val;
-    last_switch_val <= sw[0];
+    sos_trigger <= sw[6] & !last_switch_val;
+    last_switch_val <= sw[6];
   end
 
   sos_dist_calculator sos_calc(
@@ -99,14 +120,14 @@ module top_level(
     .delay_valid());
 
 
-    logic [6:0] ss_c;
-    assign ss0_c = ss_c; 
-    assign ss1_c = ss_c;
-    seven_segment_controller mssc(.clk_in(audio_clk),
-                                .rst_in(sys_rst),
-                                .val_in(calculated_delay),
-                                .cat_out(ss_c),
-                                .an_out({ss0_an, ss1_an}));
+  logic [6:0] ss_c;
+  assign ss0_c = ss_c; 
+  assign ss1_c = ss_c;
+  seven_segment_controller mssc(.clk_in(audio_clk),
+                              .rst_in(sys_rst),
+                              .val_in(calculated_delay),
+                              .cat_out(ss_c),
+                              .an_out({ss0_an, ss1_an}));
 
   logic signed [7:0] tone_440; 
   sine_generator sine_440 (
@@ -128,7 +149,8 @@ module top_level(
   assign pdm_in = sw[2] ? {tone_440[7], tone_440[7], tone_440[7], tone_440[7], 
                     tone_440[7], tone_440[7], tone_440[7], tone_440[7], tone_440[7:0]} <<< 8 : 
                     (sw[3] ? valid_audio_out_1 : 
-                    (sw[4] ? down_sampled_audio : 0));
+                    (sw[4] ? down_sampled_audio : 
+                    (sw[5] ? sos_audio_out : 0)));
 
   pdm pdm(
     .clk_in(audio_clk),
@@ -136,26 +158,6 @@ module top_level(
     .level_in(pdm_in),
     .pdm_out(sound_out)
   );
-
-  logic [15:0] filter_output;
-  logic filter_valid;
-  input_anti_alias_fir anti_alias_filter(.aclk(audio_clk),
-                                  .s_axis_data_tvalid(data_valid_out_1),
-                                  .s_axis_data_tready(1'b1),
-                                  .s_axis_data_tdata(audio_out_1),
-                                  .m_axis_data_tvalid(filter_valid),
-                                  .m_axis_data_tdata(filter_output));
-
-  logic down_sampler;
-  logic [15:0] down_sampled_audio;
-  always_ff @(posedge audio_clk) begin
-    if (filter_valid) begin
-      down_sampler <= down_sampler + 1;
-      if (down_sampler) begin
-        down_sampled_audio <= filter_output;
-      end
-    end
-  end
 
   assign spkl = sw[0] ? sound_out : 0;
   assign spkr = sw[1] ? sound_out : 0;
