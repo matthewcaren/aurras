b9794c4314dddeb81a7aa536492644a0f0668cf3
Jonas Rajagopal <jrajagop@mit.edu>
2023-11-18 23:34:29 -0500

    Done for night

--- hdl/sos_dist_calc.sv
+++ hdl/sos_dist_calc.sv
@@ -13,8 +13,8 @@
 module sos_dist_calculator #(
   parameter WINDOW_SIZE = 16,     // ~150 for most accurate, lower means less latent
   parameter MAX_DELAY = 256
-) (
-  input wire clk_in,
+) 
+( input wire clk_in,
   input wire rst_in,
   input wire step_in,
   input wire trigger,
@@ -23,31 +23,31 @@ module sos_dist_calculator #(
   output logic [7:0] delay,              // # of 24 kHz cycles
   output logic delay_valid);
 
-  typedef enum {WAITING_FOR_FIRST=1, AWAITING_IMPULSE=2, ANALYZING_RESPONSE=3, STARTING_IMPULSE = 4, DELAYING = 5} system_state;
+	typedef enum {WAITING_FOR_FIRST=1, AWAITING_IMPULSE=2, ANALYZING_RESPONSE=3, STARTING_IMPULSE = 4, DELAYING = 5} system_state;
 
-  system_state state;
+	system_state state;
 
-  logic impulse_trigger, impulse_out;
-  logic [$clog2(MAX_DELAY):0] delay_cycle_counter;      // 8-bit: 2^8=256 cycles is 3.6 meters max
+	logic impulse_trigger, impulse_out;
+	logic [$clog2(MAX_DELAY):0] delay_cycle_counter;      // 8-bit: 2^8=256 cycles is 3.6 meters max
 
-  logic [31:0] current_window_sum, prev_window_sum, prev_prev_window_sum;    // ## TODO FIGURE OUT WIDTH
-  logic [$clog2(WINDOW_SIZE):0] window_ix_counter;
+	logic [31:0] current_window_sum, prev_window_sum, prev_prev_window_sum;    // ## TODO FIGURE OUT WIDTH
+	logic [$clog2(WINDOW_SIZE):0] window_ix_counter;
 
-	logic [25:0] delay_counter;
-	localparam DELAY_CYCLES = 26'd40_000_000;
-  logic [7:0] last_delay;
-  logic [7:0] two_delays_ago;
+	logic [27:0] delay_counter;
+	localparam DELAY_CYCLES = 28'd48_000_000;
+	logic [7:0] last_delay;
+	logic [7:0] two_delays_ago;
 
-  impulse_generator imp_gen (
-  .clk_in(clk_in),
-  .rst_in(rst_in),
-  .step_in(step_in),
-  .impulse_in(impulse_trigger),
-  .impulse_out(impulse_out),
-  .amp_out(amp_out));
+	impulse_generator imp_gen (
+	.clk_in(clk_in),
+	.rst_in(rst_in),
+	.step_in(step_in),
+	.impulse_in(impulse_trigger),
+	.impulse_out(impulse_out),
+	.amp_out(amp_out));
 
 
-  always_ff @(posedge clk_in) begin
+  	always_ff @(posedge clk_in) begin
     if (rst_in) begin
         delay <= 0;
         two_delays_ago <= 0;
@@ -60,6 +60,9 @@ module sos_dist_calculator #(
     end else case (state)
             WAITING_FOR_FIRST: begin
                 if (trigger) begin
+					delay <= 0;
+					two_delays_ago <= 0;
+					last_delay <= 0;
                     state <= STARTING_IMPULSE;
                 end
                 delay_counter <= 0;
