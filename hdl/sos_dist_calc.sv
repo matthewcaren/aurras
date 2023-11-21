862f650555ede0964465dceb83ccf94b24f053d9
Jonas Rajagopal <jrajagop@mit.edu>
2023-11-21 16:41:56 -0500

    changed to 16 bits

--- hdl/sos_dist_calc.sv
+++ hdl/sos_dist_calc.sv
@@ -132,7 +132,7 @@ module sos_dist_calculator #(
                             end
                         end else begin
                             window_ix_counter <= window_ix_counter + 1;
-                            current_window_sum <= current_window_sum + (mic_in[31] ? ((~mic_in) + 1) : mic_in);
+                            current_window_sum <= current_window_sum + (mic_in[15] ? ((~mic_in) + 1) : mic_in);
                         end
 
                         delay_cycle_counter <= delay_cycle_counter + 1;
