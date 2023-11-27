34278e024255e807f91e51b6713bf1eacdc5a8b1
bryanjangeesingh <bryanjangeesingh@gmail.com>
2023-11-26 23:03:06 -0500

    frequency sweep wip1

---
+++ hdl/frequency_sweep.sv
@@ -0,0 +1,20 @@
+`timescale 1ns / 1ps
+`default_nettype none
+
+module frequency_sweep (
+  input wire clk_in,
+  input wire rst_in,
+  output wire [15:0] sound_out
+);
+
+// Paramaters to be adjusted based on desired outcome
+
+
+
+ 
+
+endmodule
+
+
+`timescale 1ns / 1ps
+`default_nettype wire
