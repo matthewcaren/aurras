f526084f2867d728cbd663798132ee0c92aa1946
bryanjangeesingh <bryanjangeesingh@gmail.com>
2023-11-26 23:13:41 -0500

    frequency sweep wip2

--- hdl/frequency_sweep.sv
+++ hdl/frequency_sweep.sv
@@ -8,8 +8,20 @@ module frequency_sweep (
 );
 
 // Paramaters to be adjusted based on desired outcome
+parameter sweep_start_freq = 1000; 
+parameter sweep_stop_freq = 20000; 
 
+logic [31:0] counter, accumulator, phase_increment = 0; 
 
+always_ff @(posedge clk_in) begin 
+    if (rst_in) begin
+        counter <= 0; 
+        accumulator <= 0;
+        phase_increment <= 0; 
+    end else begin 
+        
+    end 
+end
 
  
 
