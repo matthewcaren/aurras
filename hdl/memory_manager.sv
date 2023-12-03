7308996797573a8ab3635e8154418531c86fa282
Jonas Rajagopal <jrajagop@mit.edu>
2023-12-03 15:43:23 -0500

    memory mamanger done

--- hdl/memory_manager.sv
+++ hdl/memory_manager.sv
@@ -6,15 +6,15 @@ module memory_manager(input wire audio_clk,
                       input wire [15:0] write_addr,
                       input wire [15:0] write_data,
                       input wire [15:0] impulse_length,
-                      
+                      input wire [15:0] read_addr,
+                      output logic [15:0] read_data,                        
                       );
 
 
     xilinx_true_dual_port_read_first_2_clock_ram #(
         .RAM_WIDTH(16),
         .RAM_DEPTH(impulse_length)
-    ) 
-    impulse_memory (
+    ) impulse_memory (
         .addra(write_addr),
         .clka(clk_in),
         .wea(1'b1),
@@ -23,14 +23,14 @@ module memory_manager(input wire audio_clk,
         .regcea(1'b1),
         .rsta(rst_in),
         .douta(),
-        .addrb(),
+        .addrb(read_addr),
         .dinb(),
         .clkb(clk_in),
         .web(1'b0),
         .enb(1'b1),
         .rstb(rst_in),
         .regceb(1'b1),
-        .doutb()
+        .doutb(read_data)
     );
 
 endmodule
